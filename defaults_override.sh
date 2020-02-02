2160e2f727c22f48ea11c02f1a375687fc40ce46
Petr Korolev <sky4winder@gmail.com>
2020-02-03 01:18:03 +0800

    update defaults

--- defaults_override.sh
+++ defaults_override.sh
@@ -10,3 +10,6 @@ defaults write com.apple.Dock orientation -string right
 # https://www.quora.com/How-can-I-completely-hide-or-remove-the-Dock-in-Mac-OS-X-Yosemite
 defaults write com.apple.Dock autohide-delay -float 5 && killall Dock
 
+# make Dock super small:
+# defaults read com.apple.dock tilesize -> 64
+defaults write com.apple.dock tilesize -int 1 && killall Dock

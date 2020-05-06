5e9a19b4f5cf3886efe0968d7e5d4c833d56aab1
Petr Korolev <sky4winder@gmail.com>
2020-05-06 12:33:57 +0700

    zsh from github

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,8 @@ apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
 
+# My zsh configs
+wget https://raw.githubusercontent.com/skywinder/dotfiles/master/zshrc_linux -O ~/.zshrc
 
 #Vim and Vundle
 wget https://raw.githubusercontent.com/skywinder/dotfiles/master/vimrc -O ~/.vimrc

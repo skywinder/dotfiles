4f666bb70644a1d94f9049fa82b68586325270cf
Petr Korolev <sky4winder@gmail.com>
2020-05-06 12:32:16 +0700

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
+# Run this:
+#######
+#wget https://raw.githubusercontent.com/skywinder/dotfiles/master/bootstrap.sh && chmod +x ./bootstrap.sh && ./bootstrap.sh
+#######
+#
 #install zsh
 apt install zsh
 
@@ -5,6 +10,9 @@ apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
 
+
 #Vim and Vundle
+wget https://raw.githubusercontent.com/skywinder/dotfiles/master/vimrc -O ~/.vimrc
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall

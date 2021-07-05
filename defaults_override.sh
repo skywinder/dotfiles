804b7b9ab87fceb812077b5c60d9bf885a6be8de
Petr Korolev <sky4winder@gmail.com>
2021-07-05 18:41:16 +0700

    add user chrome details

--- defaults_override.sh
+++ defaults_override.sh
@@ -23,3 +23,21 @@ brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize su
 
 #### https://github.com/tborychowski/slack-nord-theme
 ### - run install.sh to make slack dark
+
+
+
+##########
+
+# Firefox:
+#(6) Set Firefox to look for userChrome.css at startup (updated 5/24/2019)
+
+#To make startup faster for most users, Firefox 69 will no longer look for this file automatically. You need to tell it to look. Here's how:
+
+    #In a new tab, type or paste about:config in the address bar and press Enter/Return. Click the button accepting the risk.
+    #In the search box above the list, type or paste userprof and pause while the list is filtered. If you do not see anything on the list, please ignore the rest of these instructions. You can close this tab now.
+    #Double-click the toolkit.legacyUserProfileCustomizations.stylesheets preference to switch the value from false to true.
+
+# add userChrome.css
+#Go to profiles folder in firefox, then 
+# >    mkdir chrome
+# >    ln -s ~/dotfiles/userChrome.css ./chrome/userChrome.css
